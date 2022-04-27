e4326a71dd1ddceb6e8f065a9e40d3f3c1c69444
Matt Gordon <gordon.matt@Matts-MBP.modem>
2022-04-28 01:02:57 +1000

    sidebar update

--- public/script.js
+++ public/script.js
@@ -1,66 +1,28 @@
-// Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const button = document.querySelector("#taskform > button"); // Complex CSS query
-const tasklist = document.getElementById("tasklist");
-const taskInput = document.getElementById("taskInput");
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+let fswitchBtn = document.querySelector(".f_switch");
 
-// Event listener for Button click
-// This could also be form.addEventListener("submit", function() {...} )
-button.addEventListener("click", function(event) {
-  event.preventDefault(); // Not as necessary for button, but needed for form submit
+closeBtn.addEventListener("click", ()=> {
+    sidebar.classList.toggle("open");
+    menuBtnChange(); // calling the function (optional)
+});
 
-  let task = form.elements.task.value; // could be swapped out for line below
-  //let task = taskInput.value;
+/*searchBtn.addEventListener("click", ()=> { // Sidebar open when you click on the search icon
+    sidebar.classList.toggle("open");
+    menuBtnChange(); // calling the function (optional)
+}); */
 
-  let date = (new Date()).toLocaleDateString('en-US') //Convert to short date format
+fswitchBtn.addEventListener("click", ()=> { // Sidebar open when you click on the focus icon
+    sidebar.classList.toggle("open");
+    menuBtnChange(); // calling the function (optional)
+});
 
-  // Call the addTask() function using
-  addTask(task, date, "26/03/2021", "Low", ["1", "30"], false);
-
-  // Log out the newly populated taskList everytime the button has been pressed
-  console.log(taskList);
-})
-
-// Create an empty array to store our tasks
-var taskList = [];
-
-function addTask(taskDescription, createdDate, dueDate, priorityRating, estimatedTime, completionStatus) {
-  let task = {
-    taskDescription,
-    createdDate,
-    dueDate,
-    priorityRating,
-    estimatedTime,
-    completionStatus
-  };
-
-  // Add the task to our array of tasks
-  taskList.push(task);
-
-  // Separate the DOM manipulation from the object creation logic
-  renderTask(task);
-}
-
-
-// Function to display the item on the page
-function renderTask(task) {
-  let item = document.createElement("li");
-  item.innerHTML = "<p>" + task.taskDescription + "</p>";
-
-  tasklist.appendChild(item);
-
-  // Setup delete button DOM elements
-  let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete");
-  delButton.appendChild(delButtonText);
-  item.appendChild(delButton); // Adds a delete button to every task
-
-  // Listen for when the 
-  delButton.addEventListener("click", function(event){
-    item.remove(); // Remove the task item from the page when button clicked
-    // Because we used 'let' to define the item, this will always delete the right element
-  })
-  
-  // Clear the value of the input once the task has been added to the page
-  form.reset();
+// following are the code to change sidebar button (optional)
+function menuBtnChange() {
+    if(sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); // replcaes the icons class
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right","bx-menu"); // replaces the icons class
+    }
 }
