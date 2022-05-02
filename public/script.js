a13a479b5dc499f6e20ce0aca9067a049a8120b2
mgor3944 <mgor3944@uni.sydney.edu.au>
2022-05-02 21:55:35 +1000

    finished calender - dashboard

--- public/script.js
+++ public/script.js
@@ -35,38 +35,150 @@ function menuBtnChange() {
 function focusBtnChange() {
     if(focusBtn.classList.contains("bx-bolt-circle")) {
         focusBtn.classList.replace("bx-bolt-circle", "bxs-bolt-circle"); // replaces the icons class
+        focusBtn.style.color = "#E95050";
     } else {
         focusBtn.classList.replace("bxs-bolt-circle","bx-bolt-circle"); // replaces the icons class
+        focusBtn.style.color = "#E950D1";
     }
 }
 
-//-------------------------//
-// Calender Customisations //
-//-------------------------//
+//-----------------//
+// TOP NAVBAR DATE //
+//-----------------//
 
-let dashdate = new Date();
+let topDate = new Date();
 
-// calculating date aspects
+let currDay = document.querySelector("#current_day");
+let currDate = document.querySelector("#current_date");
 
-let year = dashdate.getFullYear();
-let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][dashdate.getMonth()];
-let date = dashdate.getDate();
-let day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][dashdate.getDay() - 1];
+let cMonth = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+][topDate.getMonth()];
 
-isLeapYear = (year) => {
-    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+let cWDay = [
+    "Monday",
+    "Tuesday",
+    "Wednesday", 
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+][topDate.getDay() - 1];
+
+// Setting Top Navbar Current Date (Custom Display)
+currDay.innerHTML = cWDay + ", ";
+currDate.innerHTML = topDate.getDate() + " " + cMonth + " " + topDate.getFullYear(); 
+
+//--------------------//
+// DASHBOARD CALENDER //
+//--------------------//
+
+let date = new Date();
+
+let renderCalendar = () => {
+    date.setDate(1);
+
+    // Attatching JS to HTML elements
+
+    let calDays = document.querySelector(".days");
+    
+    // Calender Calculations
+
+    let lastDay = new Date( // calc last day of current month
+        date.getFullYear(), 
+        date.getMonth() + 1, 
+        0
+    ).getDate(); 
+
+    let prevLastDay = new Date(  // calc last day of last month
+        date.getFullYear(),
+        date.getMonth(),
+        0
+    ).getDate();
+
+    let firstDayIndex = date.getDay(); // calc first day of month
+
+    let lastDayIndex = new Date( // calc last day of current week
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDay();
+
+    let nextDays = 7 - lastDayIndex - 1; // calc how many days of next month to inc
+    
+    // Setting up months and weekdays
+
+    let months = [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June", 
+        "July", 
+        "August", 
+        "September", 
+        "October", 
+        "November", 
+        "December"
+    ][date.getMonth()];
+    
+    let weekday = [
+        "Monday",
+        "Tuesday",
+        "Wednesday", 
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ][date.getDay()];
+    
+    // Adding current Month/Year to Calender Top
+    document.querySelector("#currMonth").innerHTML = months;
+    document.querySelector("#currYear").innerHTML = date.getFullYear();
+    
+    // Adding Month Days (prev, current, next) to Calender
+
+    let days = "";
+    
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div id="prev-month-days">${prevLastDay - x + 1}</div>`;
+    }
+    
+    for (let i = 1; i <= lastDay; i++) {
+        if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today">${i}</div>`;
+        } else {
+            days += `<div>${i}</div>`;
+        }
+    }
+    
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div id="next-month-days">${j}</div>`;
+        calDays.innerHTML = days;
+    }
 }
 
-getFebDays = (year) => {
-    return isLeapYear(year) ? 29 : 28
-}
+// linking calender nav arrows to prev and next months 
+document.querySelector('.left-arrow-cal').addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+});
 
-// Top Navbar Date
-document.getElementById("current_day").innerHTML = day + ", ";
-document.getElementById("current_date").innerHTML = date + " " + month + " " + year;
+document.querySelector('.right-arrow-cal').addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+});
 
-// Calender (element) Month & Year
-document.getElementById("curr-m-y").innerHTML =  month + " " + year;
-
-// calender dashboard function
-let calendar = document.querySelector('.calendar');
+renderCalendar();
